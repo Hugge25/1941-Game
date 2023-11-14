9943e0ff6de0bad36b57d7a243150d0723372163
Hugge25 <hugo.paulsson@hotmail.com>
2023-11-14 21:59:05 +0100

    lättsomplätt

--- 1941-Game/Assets/Scripts/WeaponController.cs
+++ 1941-Game/Assets/Scripts/WeaponController.cs
@@ -7,9 +7,33 @@ public class WeaponController : MonoBehaviour
     public GameObject bulletPrefab;
     public Transform firePoint;
     public float fireForce = 20f;
+    public int maxAmmo = 10;
+    public int currentAmmo;
+    public float reloadTime = 1f;
 
+    void Start()
+    {
+        currentAmmo = maxAmmo;
+    }
+
+    void Update()
+    {
+        if(currentAmmo <= 0)
+        {
+            ReloadGun();   
+            return;
+        }
+    }
+
+    void ReloadGun()
+    {
+        Debug.Log("Reloading...");
+        currentAmmo = maxAmmo;
+    }
     public void Fire()
     {
+        currentAmmo--;
+
         GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
         bullet.GetComponent<Rigidbody2D>().AddForce(firePoint.right * fireForce, ForceMode2D.Impulse);
     }    
